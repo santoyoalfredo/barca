05d42f7787605c401d7e6b3fb217841add922f33
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2018-02-07 18:09:49 -0500

    Added Fixtures
    - Added a base Fixture model
    - Added the admin view for fixtures

--- football/admin.py
+++ football/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Player, Team, Venue
+from .models import *
+
+class CompetitionAdmin(admin.ModelAdmin):
+	fields = ['name', 'logo']
+	list_display = ('name', 'competition_id')
+
+class FixtureAdmin(admin.ModelAdmin):
+	fields = ['season_id', 'home_team', 'away_team', 'home_score', 'away_score', 'extra_time', 'penalty_shootout', 'location',
+	 'time', 'date', 'weather', 'temperature', 'windchill', 'humidity', 'pressure', 'wind_direction', 'wind_speed']
+	list_display = ('fixture_id' , 'home_team', 'home_score', 'away_team', 'away_score', 'date', 'season_id')
+	list_editable = ('home_score', 'away_score')
 
 class PlayerAdmin(admin.ModelAdmin):
 	fields = ['name', 'full_name', 'position', 'dob', 'current_team', 'club_number', 'nationality', 'height', 'weight', 'portrait']
 	list_display = ('name', 'current_team', 'club_number', 'position', 'dob', 'nationality', 'height', 'weight', 'player_id')
 	list_editable = ('club_number', 'height', 'weight')
+
+class SeasonAdmin(admin.ModelAdmin):
+	fields = ['competition_id', 'year', 'teams']
+	list_display = ('competition_id', 'year')
 
 class TeamAdmin(admin.ModelAdmin):
 	fields = ['name', 'venue']
@@ -16,6 +30,9 @@ class VenueAdmin(admin.ModelAdmin):
 	fields = ['name', 'city', 'country', 'altitude']
 	list_display = ('name', 'city', 'country', 'altitude', 'venue_id')
 
+admin.site.register(Competition, CompetitionAdmin)
+admin.site.register(Fixture, FixtureAdmin)
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(Season, SeasonAdmin)
 admin.site.register(Team, TeamAdmin)
 admin.site.register(Venue, VenueAdmin)
--- football/models.py
+++ football/models.py
@@ -1,6 +1,63 @@
 from django.db import models
 
 # Create your models here.
+class Competition(models.Model):
+	competition_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=50)
+	logo = models.ImageField(upload_to='resources/competitions/', blank=True, default='')
+
+	def __str__(self):
+		return '%s' % (self.name)
+
+class Fixture(models.Model):
+	fixture_id = models.AutoField(primary_key=True)
+	season_id = models.ForeignKey('Season', on_delete=models.SET_NULL, null=True)
+	home_team = models.ForeignKey('Team', on_delete=models.SET_NULL, null=True, related_name="home_team")
+	away_team = models.ForeignKey('Team', on_delete=models.SET_NULL, null=True, related_name="away_team")
+	location = models.ForeignKey('Venue', on_delete=models.SET_NULL, null=True)
+	date = models.DateField(auto_now=False, auto_now_add=False)
+	time = models.TimeField(auto_now=False, auto_now_add=False)
+	weather_choices = (
+		('CLE', 'Clear'),
+		('CLO', 'Cloudy'),
+		('RAI', 'Rainy'),
+		('OVE', 'Overcast'),
+		('SUN', 'Sunny'),
+		)
+	weather = models.CharField(max_length=3, choices=weather_choices, default='CLE')
+	temperature = models.DecimalField(max_digits=3, decimal_places=1)
+	windchill = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+	humidity = models.IntegerField()
+	pressure = models.IntegerField()
+	wind_direction_choices = (
+		('N', 'North'),
+		('NNE', 'North-Northeast'),
+		('NE', 'Northeast'),
+		('ENE', 'East-Northeast'),
+		('E', 'East'),
+		('ESE', 'East-Southeast'),
+		('SE', 'Southeast'),
+		('SSE', 'South-Southeast'),
+		('S', 'South'),
+		('SSW', 'South-Southwest'),
+		('SW', 'Southwest'),
+		('WSW', 'West-Southwest'),
+		('W', 'West'),
+		('WNW', 'West-Northwest'),
+		('NW', 'Northwest'),
+		('NNW', 'North-Northwest'),
+		('V', 'Variable'),
+		)
+	wind_direction = models.CharField(max_length=3, choices=wind_direction_choices, default='V')
+	wind_speed = models.DecimalField(max_digits=3, decimal_places=1)
+	home_score = models.IntegerField()
+	away_score = models.IntegerField()
+	extra_time = models.BooleanField()
+	penalty_shootout = models.BooleanField()
+
+	def __str__(self):
+		return '%s %d-%d %s' % (self.home_team, self.home_score, self.away_score, self.away_team)
+
 class Player(models.Model):
 	player_id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=50)
@@ -18,6 +75,15 @@ class Player(models.Model):
 	def __str__(self):
 		return '%s' % (self.name)
 
+class Season(models.Model):
+	season_id = models.AutoField(primary_key=True)
+	competition_id = models.ForeignKey('Competition', on_delete=models.SET_DEFAULT, blank=True, default='')
+	year = models.IntegerField()
+	teams = models.ManyToManyField('Team')
+
+	def __str__(self):
+		return '%s %s-%s' % (self.competition_id, self.year, (self.year+1))
+
 class Team(models.Model):
 	team_id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=50)
@@ -25,7 +91,6 @@ class Team(models.Model):
 
 	def __str__(self):
 		return '%s' % (self.name)
-
 
 class Venue(models.Model):
 	venue_id = models.AutoField(primary_key=True)
