66f0f9c528ea3a00970562c645ec5bad5d412cda
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2018-05-17 13:23:49 -0400

    Further modifications for Travis CI
    - Added separate settings file for Travis
    - Added example for using secrets from another file
    - Modified Travis yaml file

---
+++ barca/secrets_example.py
@@ -0,0 +1,8 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+KEY = 'secret_key_here'
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DB_USER = 'database_username_here'
+DB_PASSWORD = 'database_password_here'
